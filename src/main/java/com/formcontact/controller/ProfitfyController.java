d75c92376aee3e0a905303c623bb2255609837f4
yohanscar <wyohan@gmail.com>
2018-11-22 19:41:57 -0200

    fix and more

--- src/main/java/com/formcontact/controller/ProfitfyController.java
+++ src/main/java/com/formcontact/controller/ProfitfyController.java
@@ -21,14 +21,217 @@ import com.formcontact.model.ContatoModel;
 	@RestController
 
     public class ProfitfyController {
-	   
+		
+		
+		
+		
+		
 		//RouteSeq: 15
-	    @RequestMapping(method=RequestMethod.GET, value="/profitfy/{x}")
-	    public ResponseEntity<String> cadastrarContato(@PathVariable String x){
+	    @RequestMapping(method=RequestMethod.GET, value="/profitfyBrltoDcr/")
+	    public ResponseEntity<String> brlDcr(){
 			String responseString = "";
-		    //https://profitfy.trade/api/v1/public/orderbook/BRL/DCR
-			String url = "https://profitfy.trade/api/v1/public/orderbook/";
-			String getUrl = url + x;
+
+			String getUrl = "https://profitfy.trade/api/v1/public/orderbook/BRL/DCR";
+
+			CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+			HttpGet request = new HttpGet(getUrl);
+
+			HttpResponse response = null;
+	
+			try {             
+	
+				response = httpClient.execute(request);
+				System.out.println(response.toString());
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			try {
+				responseString = new BasicResponseHandler().handleResponse(response);				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}finally {
+				try {
+					httpClient.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+			System.out.println(responseString);	
+	
+	        return new ResponseEntity<String>(responseString, HttpStatus.CREATED);
+	    }
+	   
+		
+	    @RequestMapping(method=RequestMethod.GET, value="/profitfyBrltoLtc/")
+	    public ResponseEntity<String> brlLtc(){
+			String responseString = "";
+
+			String getUrl = "https://profitfy.trade/api/v1/public/orderbook/BRL/LTC";
+
+			CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+			HttpGet request = new HttpGet(getUrl);
+
+			HttpResponse response = null;
+	
+			try {             
+	
+				response = httpClient.execute(request);
+				System.out.println(response.toString());
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			try {
+				responseString = new BasicResponseHandler().handleResponse(response);				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}finally {
+				try {
+					httpClient.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+			System.out.println(responseString);	
+	
+	        return new ResponseEntity<String>(responseString, HttpStatus.CREATED);
+	    }
+	   
+		
+	    @RequestMapping(method=RequestMethod.GET, value="/profitfyBrltoBtc/")
+	    public ResponseEntity<String> brlBtc(){
+			String responseString = "";
+
+			String getUrl = "https://profitfy.trade/api/v1/public/orderbook/BRL/BTC";
+
+			CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+			HttpGet request = new HttpGet(getUrl);
+
+			HttpResponse response = null;
+	
+			try {             
+	
+				response = httpClient.execute(request);
+				System.out.println(response.toString());
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			try {
+				responseString = new BasicResponseHandler().handleResponse(response);				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}finally {
+				try {
+					httpClient.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+			System.out.println(responseString);	
+	
+	        return new ResponseEntity<String>(responseString, HttpStatus.CREATED);
+	    }
+	   
+		
+	    @RequestMapping(method=RequestMethod.GET, value="/profitfyBtctoLtc/")
+	    public ResponseEntity<String> btcLtc(){
+			String responseString = "";
+
+			String getUrl = "https://profitfy.trade/api/v1/public/orderbook/BTC/LTC";
+
+			CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+			HttpGet request = new HttpGet(getUrl);
+
+			HttpResponse response = null;
+	
+			try {             
+	
+				response = httpClient.execute(request);
+				System.out.println(response.toString());
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			try {
+				responseString = new BasicResponseHandler().handleResponse(response);				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}finally {
+				try {
+					httpClient.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+			System.out.println(responseString);	
+	
+	        return new ResponseEntity<String>(responseString, HttpStatus.CREATED);
+	    }
+	   
+		
+	    @RequestMapping(method=RequestMethod.GET, value="/profitfyBtctoDcr/")
+	    public ResponseEntity<String> btcDcr(){
+			String responseString = "";
+
+			String getUrl = "https://profitfy.trade/api/v1/public/orderbook/BTC/DCR";
+
+			CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+			HttpGet request = new HttpGet(getUrl);
+
+			HttpResponse response = null;
+	
+			try {             
+	
+				response = httpClient.execute(request);
+				System.out.println(response.toString());
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			try {
+				responseString = new BasicResponseHandler().handleResponse(response);				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}finally {
+				try {
+					httpClient.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+			System.out.println(responseString);	
+	
+	        return new ResponseEntity<String>(responseString, HttpStatus.CREATED);
+	    }
+	   
+		
+	    @RequestMapping(method=RequestMethod.GET, value="/profitfyLtctoDcr/")
+	    public ResponseEntity<String> LtcDcr(){
+			String responseString = "";
+
+			String getUrl = "https://profitfy.trade/api/v1/public/orderbook/LTC/DCR";
 
 			CloseableHttpClient httpClient = HttpClientBuilder.create().build();
 
