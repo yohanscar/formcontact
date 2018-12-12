f5906a6da480bf81a6cdab98afb6971057d9abf6
yohanscar <wyohan@gmail.com>
2018-12-12 21:18:43 -0200

    comments

--- src/main/java/com/formcontact/controller/ContatoController.java
+++ src/main/java/com/formcontact/controller/ContatoController.java
@@ -29,22 +29,22 @@ import com.formcontact.service.ContatoService;
 	        return new ResponseEntity<ContatoModel>(contatoCadastrado, HttpStatus.CREATED);
 	    }
 	   
-		//RouteSeq: 16
-	    @RequestMapping(method=RequestMethod.GET, value="/contatos", produces=MediaType.APPLICATION_JSON_VALUE)
-	    public ResponseEntity<Collection<ContatoModel>> buscarTodosforms(){
-	        Collection<ContatoModel> contatosBuscados = ContatoService.buscarTodos();
-	        return new ResponseEntity<>(contatosBuscados, HttpStatus.OK);
-		}
+		// //RouteSeq: 16
+	    // @RequestMapping(method=RequestMethod.GET, value="/contatos", produces=MediaType.APPLICATION_JSON_VALUE)
+	    // public ResponseEntity<Collection<ContatoModel>> buscarTodosforms(){
+	    //     Collection<ContatoModel> contatosBuscados = ContatoService.buscarTodos();
+	    //     return new ResponseEntity<>(contatosBuscados, HttpStatus.OK);
+		// }
 		
-		//RouteSeq: 18
-	    @RequestMapping(method=RequestMethod.DELETE, value="/contato/{id}")
-	    public ResponseEntity<ContatoModel> excluirContato(@PathVariable Integer id){
-	        ContatoModel contatoEncontrado = ContatoService.buscarPorId(id);
-	        if(contatoEncontrado==null){
-	            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-	        }
-	        ContatoService.excluir(contatoEncontrado);
-	        return new ResponseEntity<ContatoModel>(HttpStatus.OK);
-		}
+		// //RouteSeq: 18
+	    // @RequestMapping(method=RequestMethod.DELETE, value="/contato/{id}")
+	    // public ResponseEntity<ContatoModel> excluirContato(@PathVariable Integer id){
+	    //     ContatoModel contatoEncontrado = ContatoService.buscarPorId(id);
+	    //     if(contatoEncontrado==null){
+	    //         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	    //     }
+	    //     ContatoService.excluir(contatoEncontrado);
+	    //     return new ResponseEntity<ContatoModel>(HttpStatus.OK);
+		// }
 		
 	}
