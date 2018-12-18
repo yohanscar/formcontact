33984e902aa83cbe2e3a5f9bf5f323ca13fde250
yohanscar <wyohan@gmail.com>
2018-12-17 23:56:02 -0200

    sta

--- src/main/java/com/formcontact/util/WebConfig.java
+++ src/main/java/com/formcontact/util/WebConfig.java
@@ -21,28 +21,28 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		registry.addMapping("/**/**");
 	}
 
-	public EmbeddedServletContainerFactory servletContainer() {
-		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
-			@Override
-			protected void postProcessContext(Context context) {
-			  SecurityConstraint securityConstraint = new SecurityConstraint();
-			  securityConstraint.setUserConstraint("CONFIDENTIAL");
-			  SecurityCollection collection = new SecurityCollection();
-			  collection.addPattern("/*");
-			  securityConstraint.addCollection(collection);
-			  context.addConstraint(securityConstraint);
-			}
-		  };
+	// public EmbeddedServletContainerFactory servletContainer() {
+	// 	TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+	// 		@Override
+	// 		protected void postProcessContext(Context context) {
+	// 		  SecurityConstraint securityConstraint = new SecurityConstraint();
+	// 		  securityConstraint.setUserConstraint("CONFIDENTIAL");
+	// 		  SecurityCollection collection = new SecurityCollection();
+	// 		  collection.addPattern("/*");
+	// 		  securityConstraint.addCollection(collection);
+	// 		  context.addConstraint(securityConstraint);
+	// 		}
+	// 	  };
 		 
-		tomcat.addAdditionalTomcatConnectors(redirectConnector());
-		return tomcat;
-	  }
-	private Connector redirectConnector() {
-		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-		connector.setScheme("http");
-		connector.setPort(8080);
-		connector.setSecure(false);
-		connector.setRedirectPort(8443);
-		return connector;
-	  }
+	// 	tomcat.addAdditionalTomcatConnectors(redirectConnector());
+	// 	return tomcat;
+	//   }
+	// private Connector redirectConnector() {
+	// 	Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+	// 	connector.setScheme("http");
+	// 	connector.setPort(8080);
+	// 	connector.setSecure(false);
+	// 	connector.setRedirectPort(8443);
+	// 	return connector;
+	//   }
 }
