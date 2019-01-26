480071438106e818972144354dea36eb32db9032
Yohan <wyohan@gmail.com>
2019-01-26 20:38:03 -0200

    bug fix

--- src/main/java/com/formcontact/util/HMACFactory.java
+++ src/main/java/com/formcontact/util/HMACFactory.java
@@ -43,7 +43,7 @@ public class HMACFactory {
 		
 		requestContentBase64String = Base64.encodeBase64String(md5Content.getBytes());
 
-		String signatureRawData = "8415c4fb-f7cd-471d-ac73-5ffbb586db7f" + "HttpPost" + requestUri + requestTimeStamp +nonce +requestContentBase64String;
+		String signatureRawData = "8415c4fb-f7cd-471d-ac73-5ffbb586db7f" + "POST" + requestUri + requestTimeStamp +nonce +requestContentBase64String;
 		String x = "8415c4fb-f7cd-471d-ac73-5ffbb586db7f";
 
 		String secretKeyByteArray = Base64.decodeBase64(x).toString();
